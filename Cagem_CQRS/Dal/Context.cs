9d7a9823f5023b6467590a54ab9292b967012556
ilayda <ilaydaozken@gmail.com>
2023-07-22 11:44:11 +0300

    Add project files.

---
+++ Cagem_CQRS/Dal/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Cagem_CQRS.Dal
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-AKOHCIC;initial catalog=CqrsDesignPattern;integrated security=true");
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
